982c97f5baeb2372076f5b7ed66dee4244765674
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-07-19 00:16:02 +0200

    Update code.js

--- code.js
+++ code.js
@@ -9,5 +9,6 @@ var climbStairs = function(n) {
   if (n  == 1 || n == 0) {
     return 1;
   }
+  // the recursion is being applied on the subproblem.
   return climbStairs(n - 1) + climbStairs(n - 2) + climbStairs(n - 3)
 };
